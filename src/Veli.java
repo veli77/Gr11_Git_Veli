71f062de61628ae91cbb7a56cdf507f520e5fe70
Veli Ozerdem <veliozerdem@gmail.com>
2022-12-15 22:03:17 +0300

    my last commit work is done

--- src/Veli.java
+++ src/Veli.java
@@ -2,5 +2,6 @@ public class Veli {
     public static void main(String[] args) {
         System.out.println("my first change");
         System.out.println("my second change");
+        System.out.println("my last change");
     }
 }
