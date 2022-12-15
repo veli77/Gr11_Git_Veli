b3b08c76198232fe703bdb2389960db2a4b7a48d
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-15 22:33:23 +0300

    last commit from Atik branch

--- src/Atik.java
+++ src/Atik.java
@@ -1,5 +1,6 @@
 public class Atik {
     public static void main(String[] args) {
         System.out.println("first change from Atik branch");
+        System.out.println("last change from Atik branch");
     }
 }
