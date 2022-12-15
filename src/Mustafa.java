87b4de1990d8bcc10d9c96edbb3bddd4bc62e5d0
Mustafa MUEZZINOGLU <mustafamuezzinoglu@gmail.com>
2022-12-15 22:33:39 +0300

    last commit from Mustafa branch

--- src/Mustafa.java
+++ src/Mustafa.java
@@ -1,5 +1,7 @@
 public class Mustafa {
     public static void main(String[] args) {
         System.out.println("first change from Mustafa branch");
+        System.out.println("last change from Mustafa branch");
+
     }
 }
