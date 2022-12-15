eed81f2893ec3ea35a3ef68c4b93a3cb098e5f8a
Veli Ozerdem <veliozerdem@gmail.com>
2022-12-15 22:55:49 +0300

    Conflict veli commit

---
+++ src/Conflict.java
@@ -0,0 +1,10 @@
+public class Conflict {
+    public static void main(String[] args) {
+        System.out.println("Conflict changes from Veli's branch");
+        System.out.println("Conflict changes from Veli's branch");
+        System.out.println("Conflict changes from Veli's branch");
+        System.out.println("Conflict changes from Veli's branch");
+        System.out.println("Conflict changes from Veli's branch");
+        System.out.println("Conflict changes from Veli's branch");
+    }
+}
